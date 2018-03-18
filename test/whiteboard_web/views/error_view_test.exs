910b7e55429069c3c2095f5ad519e6e795b575c5
Dan Bruder <dbruder@rapiddg.com>
2018-03-18 15:10:26 -0400

    init

---
+++ test/whiteboard_web/views/error_view_test.exs
@@ -0,0 +1,21 @@
+defmodule WhiteboardWeb.ErrorViewTest do
+  use WhiteboardWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(WhiteboardWeb.ErrorView, "404.html", []) ==
+           "Page not found"
+  end
+
+  test "render 500.html" do
+    assert render_to_string(WhiteboardWeb.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(WhiteboardWeb.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+end
